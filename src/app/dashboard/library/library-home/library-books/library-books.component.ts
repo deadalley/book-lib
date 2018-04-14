e2b3bedb1da8b0d89841d195cfd255424b8e8905
deadalley <jessica.aissa@live.com>
2018-04-14 20:35:10 +0200

    Change ordering of library according to selection

--- src/app/dashboard/library/library-home/library-books/library-books.component.ts
+++ src/app/dashboard/library/library-home/library-books/library-books.component.ts
@@ -11,12 +11,52 @@ import { LibraryService } from '../../library.service'
 
 export class LibraryBooksComponent implements OnInit {
   tilesDisplay = true
+  orderingMethod = 'title'
+  orderedItems: any
   books: Book[]
 
   constructor(private libraryService: LibraryService) {
-    libraryService.books$.subscribe((books) => this.books = books)
+    libraryService.books$.subscribe((books) => {
+      this.books = books
+      this.books.push({
+        id: 157,
+        title: 'TITLE',
+        author: 'AUTHOR',
+        owned: true,
+        read: true,
+        favorite: false,
+        date: (new Date()).toISOString()
+      })
+      this.books.push({
+        id: 157,
+        title: 'TITLE22',
+        author: 'AUTHOR',
+        owned: true,
+        read: true,
+        favorite: false,
+        date: (new Date()).toISOString()
+      })
+    })
     libraryService.tilesDisplay$.subscribe((tilesDisplay) => this.tilesDisplay = tilesDisplay)
+    libraryService.bookOrdering$.subscribe((ordering) => {
+      this.orderBooks(ordering)
+      console.log(this.orderedItems)
+      this.orderingMethod = ordering
+    })
   }
 
   ngOnInit() { }
+
+  private orderBooks(ordering) {
+    this.orderedItems = { }
+    if (ordering === 'title') { return }
+
+    this.books.forEach((book) => {
+      if (Object.keys(this.orderedItems).includes(book[ordering])) {
+        this.orderedItems[book[ordering]].push(book)
+      } else {
+        this.orderedItems[book[ordering]] = [book]
+      }
+    })
+  }
 }
--- src/app/dashboard/library/library.service.ts
+++ src/app/dashboard/library/library.service.ts
@@ -7,10 +7,12 @@ import { Collection } from '../../../interfaces/collection'
 
 @Injectable()
 export class LibraryService {
+  private bookOrdering = new BehaviorSubject<string>('title')
   private tilesDisplay = new BehaviorSubject<boolean>(true)
   private books = new BehaviorSubject<Book[]>([])
   private collections = new BehaviorSubject<Collection[]>([])
 
+  bookOrdering$ = this.bookOrdering.asObservable()
   tilesDisplay$ = this.tilesDisplay.asObservable()
   collections$ = this.collections.asObservable()
   books$ = this.books.asObservable()
@@ -27,5 +29,9 @@ export class LibraryService {
 
   toggleTilesDisplay(toggle: boolean) {
     this.tilesDisplay.next(toggle)
+  }
+
+  setBookOrderingMethod(method: string) {
+    this.bookOrdering.next(method)
   }
 }
