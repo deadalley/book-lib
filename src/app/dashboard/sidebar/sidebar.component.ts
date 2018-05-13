af460eeca1703ed3a7be548947ababe2552de386
deadalley <jessica.aissa@live.com>
2018-05-13 14:18:13 +0200

    Implemented sign out

--- src/app/dashboard/sidebar/sidebar.component.ts
+++ src/app/dashboard/sidebar/sidebar.component.ts
@@ -1,39 +1,46 @@
 import { Component, OnInit } from '@angular/core'
+import { AuthService } from 'services/auth.service';
 
 declare var $: any
 
 export interface RouteInfo {
-    path: string
-    title: string
-    icon: string
-    class: string
+  path: string
+  title: string
+  icon: string
+  class: string
 }
 
 export const ROUTES: RouteInfo[] = [
   { path: 'home', title: 'Home',  icon: 'pe-7s-home', class: '' },
   { path: 'library', title: 'Library',  icon: 'pe-7s-notebook', class: '' },
-  { path: 'author_watch', title: 'Author Watch',  icon: 'pe-7s-look', class: '' },
-  { path: 'wishlist', title: 'Wishlist',  icon: 'pe-7s-gift', class: '' },
-  { path: 'profile', title: 'Profile',  icon: 'pe-7s-user', class: '' }
+  { path: 'profile', title: 'Profile',  icon: 'pe-7s-user', class: '' },
+  { path: 'sign-out', title: 'Log out', icon: 'pe-7s-power', class: '' }
 ]
 
 @Component({
-    moduleId: module.id,
-    selector: 'sidebar-cmp',
-    templateUrl: 'sidebar.component.html',
+  moduleId: module.id,
+  selector: 'sidebar-cmp',
+  templateUrl: 'sidebar.component.html',
 })
 
 export class SidebarComponent implements OnInit {
-    public menuItems: any[]
+  public menuItems: any[]
 
-    ngOnInit() {
-        this.menuItems = ROUTES.filter(menuItem => menuItem)
-    }
+  constructor(private authService: AuthService) { }
 
-    isNotMobileMenu(){
-        if ($(window).width() > 991){
-            return false
-        }
-        return true
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem)
+    this.menuItems.find((item) => item.path === 'sign-out').action = this.signOut()
+  }
+
+  isNotMobileMenu() {
+    if ($(window).width() > 991) {
+      return false
     }
+    return true
+  }
+
+  signOut() {
+    this.authService.logout()
+  }
 }
