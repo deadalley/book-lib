5ee0055a1d4e98b756e9d47a622ec935648392ba
deadalley <jessica.aissa@live.com>
2018-05-13 18:36:26 +0200

    Fixed title on navbar on dashboard

--- src/app/dashboard/shared/navbar/navbar.component.ts
+++ src/app/dashboard/shared/navbar/navbar.component.ts
@@ -4,55 +4,49 @@ import { Router, ActivatedRoute } from '@angular/router'
 import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common'
 
 @Component({
-    moduleId: module.id,
-    selector: 'navbar-cmp',
-    templateUrl: 'navbar.component.html'
+  moduleId: module.id,
+  selector: 'navbar-cmp',
+  templateUrl: 'navbar.component.html'
 })
 
-export class NavbarComponent implements OnInit{
-    private listTitles: any[]
-    location: Location
-    private nativeElement: Node
-    private toggleButton
-    private sidebarVisible: boolean
+export class NavbarComponent implements OnInit {
+  private listTitles: any[]
+  private nativeElement: Node
+  private toggleButton
+  private sidebarVisible: boolean
 
-    @ViewChild('navbar-cmp') button
+  @ViewChild('navbar-cmp') button
 
-    constructor(location: Location, private renderer: Renderer, private element: ElementRef) {
-        this.location = location
-        this.nativeElement = element.nativeElement
-        this.sidebarVisible = false
-    }
+  constructor(location: Location, private router: Router, private renderer: Renderer, private element: ElementRef) {
+    this.nativeElement = element.nativeElement
+    this.sidebarVisible = false
+  }
 
-    ngOnInit(){
-        this.listTitles = ROUTES.filter(listTitle => listTitle)
-        const navbar: HTMLElement = this.element.nativeElement
-        this.toggleButton = navbar.getElementsByClassName('navbar-toggle')[0]
-    }
-    getTitle(){
-        let titlee = window.location.pathname
-        titlee = titlee.substring(11)
-        for (let item = 0; item < this.listTitles.length; item++){
-            if (this.listTitles[item].path === titlee){
-                return this.listTitles[item].title
-            }
-        }
-        return 'Dashboard'
-    }
-    sidebarToggle(){
-        const toggleButton = this.toggleButton
-        const body = document.getElementsByTagName('body')[0]
+  ngOnInit() {
+    this.listTitles = ROUTES.filter(listTitle => listTitle)
+    const navbar: HTMLElement = this.element.nativeElement
+    this.toggleButton = navbar.getElementsByClassName('navbar-toggle')[0]
+  }
 
-        if (this.sidebarVisible == false){
-            setTimeout(function(){
-                toggleButton.classList.add('toggled')
-            }, 500)
-            body.classList.add('nav-open')
-            this.sidebarVisible = true
-        } else {
-            this.toggleButton.classList.remove('toggled')
-            this.sidebarVisible = false
-            body.classList.remove('nav-open')
-        }
+  getTitle() {
+    const splitUrl = this.router.url.split('/')
+    return splitUrl[2].replace(/^\w/, (chr) => chr.toUpperCase())
+  }
+
+  sidebarToggle() {
+    const toggleButton = this.toggleButton
+    const body = document.getElementsByTagName('body')[0]
+
+    if (this.sidebarVisible === false) {
+      setTimeout(function(){
+        toggleButton.classList.add('toggled')
+      }, 500)
+      body.classList.add('nav-open')
+      this.sidebarVisible = true
+    } else {
+      this.toggleButton.classList.remove('toggled')
+      this.sidebarVisible = false
+      body.classList.remove('nav-open')
     }
+  }
 }
