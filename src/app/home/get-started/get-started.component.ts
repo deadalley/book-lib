b4417b8304f2ee1bb1237fe5d5740b4179d4ceda
deadalley <jessica.aissa@live.com>
2018-05-13 12:23:30 +0200

    Created basic login page

---
+++ src/app/home/get-started/get-started.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core'
+import { AuthService } from '../../../services/auth.service'
+
+@Component({
+  moduleId: module.id,
+  selector: 'get-started',
+  templateUrl: 'get-started.component.html',
+  styleUrls: ['get-started.component.css']
+})
+
+export class GetStartedComponent implements OnInit {
+  errorMessage: string
+
+  @Output() loadComponent = new EventEmitter<string>()
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() { }
+
+  loginGoogle() {
+    this.authService.loginGoogle()
+  }
+
+  loginFacebook() {
+
+  }
+
+  loginEmail() {
+    this.loadComponent.emit('signin')
+  }
+ }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,12 @@ import { AuthService } from '../../services/auth.service'
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  componentToLoad = 'getstarted'
+
+  set setComponentToLoad(component: string) {
+    this.componentToLoad = component
+  }
+
   constructor(private authService: AuthService) { }
 
   ngOnInit() { }
