9be4a2cb5b6e06c213172ae0d1e841aac8ec86af
deadalley <jessica.aissa@live.com>
2018-05-19 13:03:42 +0200

    Added basic profile page

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
+import { CoreModule } from './core/core.module'
 import { SidebarModule } from './sidebar/sidebar.module'
 import { FooterModule } from './shared/footer/footer.module'
 import { NavbarModule} from './shared/navbar/navbar.module'
@@ -9,23 +11,28 @@ import { DashboardHomeModule } from './dashboard-home/dashboard-home.module'
 
 import { DashboardComponent } from './dashboard.component'
 import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component'
+import { ProfileComponent } from './profile/profile.component'
 
 const dashboardRoutes: Routes = [
   { path: '', component: DashboardComponent, children: [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: DashboardHomeComponent },
     { path: 'library', loadChildren: './library/library.module#LibraryModule' },
-    { path: 'profile', redirectTo: 'library', pathMatch: 'full' },
+    { path: 'profile', component: ProfileComponent },
   ]}
 ]
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    ProfileComponent
   ],
   imports: [
     RouterModule.forChild(dashboardRoutes),
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CoreModule,
     SidebarModule,
     NavbarModule,
     FooterModule,
---
+++ src/app/dashboard/profile/profile.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, Input, Output, EventEmitter, trigger, transition, style, animate, group, state, OnDestroy } from '@angular/core'
+import { FormControl , FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms'
+import { DatabaseService } from '../../../services/database.service'
+
+@Component({
+  moduleId: module.id,
+  // tslint:disable-next-line:component-selector
+  selector: 'profile',
+  templateUrl: 'profile.component.html',
+  styleUrls: ['./profile.component.css'],
+  animations: [
+    trigger('card', [
+      state('*', style({
+        '-ms-transform': 'translate3D(0px, 0px, 0px)',
+        '-webkit-transform': 'translate3D(0px, 0px, 0px)',
+        '-moz-transform': 'translate3D(0px, 0px, 0px)',
+        '-o-transform': 'translate3D(0px, 0px, 0px)',
+        transform: 'translate3D(0px, 0px, 0px)',
+        opacity: 1})),
+        transition('void => *', [
+          style({opacity: 0,
+            '-ms-transform': 'translate3D(0px, 150px, 0px)',
+            '-webkit-transform': 'translate3D(0px, 150px, 0px)',
+            '-moz-transform': 'translate3D(0px, 150px, 0px)',
+            '-o-transform': 'translate3D(0px, 150px, 0px)',
+            transform: 'translate3D(0px, 150px, 0px)',
+          }),
+          animate('0.3s 0s ease-out')
+        ])
+    ])
+  ]
+})
+
+export class ProfileComponent implements OnInit, OnDestroy {
+  form: FormGroup
+
+  constructor(
+    private fb: FormBuilder,
+    private databaseService: DatabaseService,
+  ) {
+    this.form = this.fb.group({
+      name: '',
+    })
+  }
+
+  ngOnInit() { }
+
+  ngOnDestroy() { }
+
+  submit(formValues) {
+    console.log('Updating user', formValues)
+  }
+}
