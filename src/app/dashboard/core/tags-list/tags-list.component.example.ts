1c5e31f6ac85e34858c7098e7e6e3f84d6d112f5
deadalley <jessica.aissa@live.com>
2019-01-22 23:34:31 -0200

    Create new tags list component

---
+++ src/app/dashboard/core/tags-list/tags-list.component.example.ts
@@ -0,0 +1,20 @@
+import { CommonModule, APP_BASE_HREF } from '@angular/common'
+import { storiesOf, moduleMetadata } from '@storybook/angular'
+import { UiService } from '../../../../services/ui.service'
+import { TagsListComponent } from './tags-list.component'
+
+const tags = ['tag1', 'tag2', 'tag3', 'the best tag']
+
+storiesOf('Tags List', module)
+  .addDecorator(
+    moduleMetadata({
+      providers: [{ provide: APP_BASE_HREF, useValue: '/' }, UiService],
+      imports: [CommonModule],
+    })
+  )
+  .add('default', () => ({
+    component: TagsListComponent,
+    props: {
+      tags,
+    },
+  }))
--- src/app/dashboard/core/tags-list/tags-list.component.ts
+++ src/app/dashboard/core/tags-list/tags-list.component.ts
@@ -4,12 +4,23 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'
   moduleId: module.id,
   selector: 'tags-list',
   templateUrl: 'tags-list.component.html',
-  styleUrls: [],
+  styleUrls: ['./tags-list.component.css'],
 })
-
 export class TagsListComponent implements OnInit {
   @Input() tags: string[]
-  @Output() selectedTag = new EventEmitter<string>()
+  @Output() selectedTags = new EventEmitter<object>()
+  _selectedTags = {}
 
-  ngOnInit() { }
+  ngOnInit() {
+    this._selectedTags = this.tags.reduce((value, tag) => {
+      value[tag] = false
+      return value
+    }, {})
+    this.selectedTags.emit(this._selectedTags)
+  }
+
+  onClick(tag: string) {
+    this._selectedTags[tag] = !this._selectedTags[tag]
+    this.selectedTags.emit(this._selectedTags)
+  }
 }
