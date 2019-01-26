3c5d5350e2ed085f71036f3342162baa99287dea
deadalley <jessica.aissa@live.com>
2019-01-26 09:26:21 -0200

    Select language on key up with buffer

--- src/app/dashboard/core/language-selector/language-selector.component.ts
+++ src/app/dashboard/core/language-selector/language-selector.component.ts
@@ -8,6 +8,9 @@ import {
   ViewChildren,
 } from '@angular/core'
 import { LANGUAGES } from 'utils/constants'
+import { BehaviorSubject } from 'rxjs'
+import { map, debounceTime } from 'rxjs/operators'
+import { upperCaseFirstLetter } from 'utils/helpers'
 
 @Component({
   selector: 'language-selector',
@@ -22,11 +25,32 @@ export class LanguageSelectorComponent implements OnInit {
   @ViewChild('languageList') languageList
   @ViewChildren('languages') languagesRef
 
-  ngOnInit() {}
+  languageInputBuffer = ''
+  languageInput = new BehaviorSubject<string>(this.languageInputBuffer)
+  languageInput$ = this.languageInput.asObservable()
 
-  onKeyDown(event) {
-    const key = event.key.toUpperCase()
-    const index = this.languages.findIndex(language => language.startsWith(key))
+  ngOnInit() {
+    this.languageInput$
+      .pipe(
+        debounceTime(300),
+        map(input => upperCaseFirstLetter(input))
+      )
+      .subscribe(input => {
+        this.findLanguage(input)
+        this.languageInputBuffer = ''
+      })
+  }
+
+  onKeyUp(event) {
+    this.languageInput.next(
+      (this.languageInputBuffer = this.languageInputBuffer.concat(event.key))
+    )
+  }
+
+  findLanguage(input: string) {
+    const index = this.languages.findIndex(language =>
+      language.startsWith(input)
+    )
 
     if (index === -1) {
       return
