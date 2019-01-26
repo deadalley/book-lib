d6f83a791843d2d09cf84c6a2c7c69fb2c45d338
deadalley <jessica.aissa@live.com>
2019-01-26 08:39:12 -0200

    Choose language on keydown

--- src/app/dashboard/core/language-selector/language-selector.component.ts
+++ src/app/dashboard/core/language-selector/language-selector.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core'
 import { LANGUAGES } from 'utils/constants'
 
 @Component({
@@ -6,11 +14,25 @@ import { LANGUAGES } from 'utils/constants'
   templateUrl: './language-selector.component.html',
   styleUrls: ['./language-selector.component.css'],
 })
-
 export class LanguageSelectorComponent implements OnInit {
   languages = LANGUAGES
   @Input() selectedLanguage
   @Output() select = new EventEmitter<string>()
 
-  ngOnInit() { }
+  @ViewChild('languageList') languageList
+  @ViewChildren('languages') languagesRef
+
+  ngOnInit() {}
+
+  onKeyDown(event) {
+    const key = event.key.toUpperCase()
+    const index = this.languages.findIndex(language => language.startsWith(key))
+
+    if (index === -1) {
+      return
+    }
+
+    const element = this.languagesRef.toArray()[index].nativeElement
+    this.languageList.nativeElement.scrollTop = element.offsetTop
+  }
 }
