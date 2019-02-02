034a2ae2c41f3b66868f30981363e4707cd71a5f
deadalley <jessica.aissa@live.com>
2019-02-02 19:15:11 +0100

    Created authors table

---
+++ src/app/dashboard/core/authors-table/authors-table.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'
+import { ANIMATIONS } from 'utils/constants'
+import { Author } from 'models/author.model'
+
+@Component({
+  moduleId: module.id,
+  selector: 'authors-table',
+  templateUrl: 'authors-table.component.html',
+  styleUrls: ['./authors-table.component.css'],
+  animations: [ANIMATIONS.CARD],
+})
+export class AuthorsTableComponent implements OnInit {
+  @Input() sectionTitle: string
+  @Input() authors: Author[]
+  @Input() clickable: boolean
+
+  @Output() onClick = new EventEmitter<Author>()
+
+  ngOnInit() {}
+}
---
+++ src/app/dashboard/core/authors-table/authors-table.example.ts
@@ -0,0 +1,57 @@
+import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router'
+import { TooltipModule } from 'ngx-bootstrap/tooltip'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { storiesOf, moduleMetadata } from '@storybook/angular'
+import { AuthorsTableComponent } from './authors-table.component'
+import AuthorFactory from 'factories/author.factory'
+import { LibraryService } from 'services/library.service'
+import { DatabaseService } from 'services/database.service'
+import { SessionService } from 'services/session.service'
+import { AngularFireModule } from 'angularfire2'
+import {
+  AngularFireDatabase,
+  AngularFireDatabaseModule,
+} from 'angularfire2/database'
+import { environment } from 'environments/environment'
+import {
+  AngularFireStorage,
+  AngularFireStorageModule,
+} from 'angularfire2/storage'
+
+const authors = AuthorFactory.buildList(4)
+
+storiesOf('Authors Table', module)
+  .addDecorator(
+    moduleMetadata({
+      providers: [
+        DatabaseService,
+        LibraryService,
+        SessionService,
+        AngularFireDatabase,
+        AngularFireStorage,
+      ],
+      imports: [
+        CommonModule,
+        RouterModule,
+        TooltipModule.forRoot(),
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireDatabaseModule,
+        AngularFireStorageModule,
+        BrowserAnimationsModule,
+      ],
+    })
+  )
+  .add('default', () => ({
+    component: AuthorsTableComponent,
+    props: {
+      authors,
+    },
+  }))
+  .add('clickable', () => ({
+    component: AuthorsTableComponent,
+    props: {
+      authors,
+      clickable: true,
+    },
+  }))
