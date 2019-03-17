c6f97d5acb68b774436a5ef9405fdf36188d896b
deadalley <jessica.aissa@live.com>
2019-03-17 16:06:09 +0100

    Sign up component updated

--- src/app/home/sign-up/sign-up.component.ts
+++ src/app/home/sign-up/sign-up.component.ts
@@ -1,47 +1,58 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core'
-import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms'
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+} from '@angular/forms'
 import { AuthService } from '../../../services/auth.service'
 
 @Component({
   moduleId: module.id,
   selector: 'sign-up',
   templateUrl: 'sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
-
 export class SignUpComponent implements OnInit {
   form: FormGroup
 
   @Output() loadComponent = new EventEmitter<string>()
 
   constructor(private fb: FormBuilder, private authService: AuthService) {
-    this.form = this.fb.group({
-      name: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
-    }, { validator: this.confirmPassword })
+    this.form = this.fb.group(
+      {
+        name: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+      },
+      { validator: this.confirmPassword }
+    )
   }
 
-  ngOnInit() { }
+  ngOnInit() {}
 
-  confirmPassword(control: AbstractControl): { passwordMismatch: boolean } {
-    const password = control.get('password').value
-    const passwordConfirm = control.get('confirmPassword').value
+  loginGoogle() {
+    this.authService.loginGoogle()
+  }
 
-    if (password !== passwordConfirm) {
-      control.get('confirmPassword').setErrors({ passwordMismatch: true})
-      return { passwordMismatch: true }
-    }
-
-    return null
+  loginFacebook() {
+    this.authService.loginFacebook()
   }
 
   signUp(formValues) {
     this.authService.signUpWithEmail(formValues)
   }
 
-  goBack() {
-    this.loadComponent.emit('signin')
+  confirmPassword(control: AbstractControl): { passwordMismatch: boolean } {
+    const password = control.get('password').value
+    const passwordConfirm = control.get('confirmPassword').value
+
+    if (password !== passwordConfirm) {
+      control.get('confirmPassword').setErrors({ passwordMismatch: true })
+      return { passwordMismatch: true }
+    }
+
+    return null
   }
 }
