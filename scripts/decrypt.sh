e1d0a672d5c9ec14f6f098201474fcc7b4eb04e5
deadalley <jessica.aissa@live.com>
2019-10-26 10:53:21 +0200

    Add openssl version

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -3,6 +3,7 @@
 PASSPHRASE=${SSL_PASSWORD}
 DIR=./src/environments
 
+openssl version
 for FILE in $(ls "${DIR}"/*.ssl); do
     echo "Decrypting $FILE to ${FILE%.ssl}"
     echo openssl aes-256-cbc -d -a -salt -md sha256 -pass pass:"$PASSPHRASE" -in $FILE -out ${FILE%.ssl}
