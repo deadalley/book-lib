5f1ed7d8442debe76f276588618a0179d65643ba
deadalley <jessica.aissa@live.com>
2019-10-26 10:50:47 +0200

    Encrypt with sha256

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -5,6 +5,6 @@ DIR=./src/environments
 
 for FILE in $(ls "${DIR}"/*.ssl); do
     echo "Decrypting $FILE to ${FILE%.ssl}"
-    echo openssl aes-256-cbc -d -a -salt -pass pass:"$PASSPHRASE" -in $FILE -out ${FILE%.ssl}
-    openssl aes-256-cbc -d -a -salt -pass pass:"$PASSPHRASE" -in $FILE -out ${FILE%.ssl}
+    echo openssl aes-256-cbc -d -a -salt -md sha256 -pass pass:"$PASSPHRASE" -in $FILE -out ${FILE%.ssl}
+    openssl aes-256-cbc -d -a -salt -md sha256 -pass pass:"$PASSPHRASE" -in $FILE -out ${FILE%.ssl}
 done
