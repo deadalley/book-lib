7727b427d524da406acdb8be7b28e797808b0c91
deadalley <jessica.aissa@live.com>
2019-10-26 10:20:25 +0200

    Echo commands before encrypt/decrypt

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -5,5 +5,6 @@ DIR=./src/environments
 
 for FILE in $(ls "$DIR"/*.ts); do
     echo "Encrypting $FILE to $FILE.ssl"
+    echo openssl aes-256-cbc -a -salt -pass pass:"$PASSPHRASE" -in $FILE -out $FILE.ssl
     openssl aes-256-cbc -a -salt -pass pass:"$PASSPHRASE" -in $FILE -out $FILE.ssl
 done
