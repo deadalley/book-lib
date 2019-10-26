8a28210c86f964d6a7a8c3dbd783f028b1e2a4db
deadalley <jessica.aissa@live.com>
2019-10-26 10:15:59 +0200

    Add ssl encrypt/decrypt files

---
+++ scripts/decrypt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PASSPHRASE=${SSL_PASSWORD}
+DIR=./src/environments
+
+for FILE in $(ls "${DIR}"/*.ssl); do
+    echo "Decrypting $FILE to ${FILE%.ssl}"
+    openssl aes-256-cbc -d -a -salt -pass pass:"$PASSPHRASE" -in $FILE -out ${FILE%.ssl}
+done
---
+++ scripts/encrypt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PASSPHRASE=${SSL_PASSWORD}
+DIR=./src/environments
+
+for FILE in $(ls "$DIR"/*.ts); do
+    echo "Encrypting $FILE to $FILE.ssl"
+    openssl aes-256-cbc -a -salt -pass pass:"$PASSPHRASE" -in $FILE -out $FILE.ssl
+done
